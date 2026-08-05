774229c377257128db46f90066ed1c09aa9be067
NUnatasha <zinyuke.n@nulondon.ac.uk>
2026-08-05 20:32:31 +0100

    adding correct code to test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def is_palindrome(input_value):
-    if input_value=="wow":
+    everything_string=str(input_value)
+    if everything_string==everything_string[::-1]
         return True
     else:
         return False
