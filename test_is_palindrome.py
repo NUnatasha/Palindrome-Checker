6d6824ed61b6dc343fa84baf0f26beb83021e07b
NUnatasha <zinyuke.n@nulondon.ac.uk>
2026-08-05 20:31:05 +0100

    adding another assert to test file

--- test_is_palindrome.py
+++ test_is_palindrome.py
@@ -2,6 +2,7 @@ from main import is_palindrome
 
 def test_is_palindrome_happy():
     assert is_palindrome("wow")==True
+    assert is_palindrome(545)==True
 
 def test_is_palindrome_unhappy():
     assert is_palindrome("favour")==False
