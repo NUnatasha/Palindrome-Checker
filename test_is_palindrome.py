f7ded70cb543bb9a5ed855a982816a6d297cc673
NUnatasha <zinyuke.n@nulondon.ac.uk>
2026-08-05 20:35:48 +0100

    adding more test examples

--- test_is_palindrome.py
+++ test_is_palindrome.py
@@ -3,6 +3,14 @@ from main import is_palindrome
 def test_is_palindrome_happy():
     assert is_palindrome("wow")==True
     assert is_palindrome(545)==True
+    assert is_palindrome(121)==True
+    assert is_palindrome("did")==True
+    assert is_palindrome("racecar")==True
+    assert is_palindrome("radar")==True
+    assert is_palindrome(5555555)==True
 
 def test_is_palindrome_unhappy():
     assert is_palindrome("favour")==False
+    assert is_palindrome("orange")==False
+    assert is_palindrome("346")==False
+    assert is_palindrome("Hello")==False
