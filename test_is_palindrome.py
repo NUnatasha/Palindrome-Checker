acd99a5655a581e6ef1ce4dfde46adcb2790ac59
NUnatasha <zinyuke.n@nulondon.ac.uk>
2026-08-05 20:28:27 +0100

    adding another function to test file

--- test_is_palindrome.py
+++ test_is_palindrome.py
@@ -2,3 +2,6 @@ from main import is_palindrome
 
 def test_is_palindrome_happy():
     assert is_palindrome("wow")==True
+
+def test_is_palindrome_unhappy():
+    assert is_palindrome("favour")==False
