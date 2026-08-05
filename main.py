02cc3885200efa1d39a2a4b9dbd9df89c4d78478
NUnatasha <zinyuke.n@nulondon.ac.uk>
2026-08-05 20:33:16 +0100

    fixing syntax error

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def is_palindrome(input_value):
     everything_string=str(input_value)
-    if everything_string==everything_string[::-1]
+    if everything_string==everything_string[::-1]:
         return True
     else:
         return False
